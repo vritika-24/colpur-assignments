6edb83a6d4ea885a8c151cc5967ae736d896408a
vritika-24 <vritikhandelwal24@gmail.com>
2026-09-05 21:44:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/HeaderComponent";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div>
+            <Header />
+
+            <main>
+                <h2>Welcome to My Website</h2>
+                <p>This is my first React application.</p>
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
